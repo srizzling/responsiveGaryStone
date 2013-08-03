49263a3fb7e85dc633e6f491001af1259083d161
Sriram Venkatesh <venksriram@gmail.com>
2013-08-02 20:54:19 -0400

    Refactored some code

---
+++ js/flashcontent.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {  
+      
+    // Check for hash value in URL  
+    var hash = window.location.hash.substr(1);  
+    var href = $('#submenu li a').each(function(){  
+        var href = $(this).attr('href');  
+        if(hash==href.substr(0,href.length-5)){  
+            var toLoad = hash+'.html #content';  
+            $('#content').load(toLoad)  
+        }   
+    });  
+      
+    $('#submenu li a').click(function(){  
+      
+    var toLoad = $(this).attr('href')+' #content';  
+    $('#content').hide('fast',loadContent);  
+    
+    $('#load').fadeIn('normal');  
+    window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5);  
+    function loadContent() {  
+        $('#content').load(toLoad,'',showNewContent())  
+    }  
+    function showNewContent() {  
+        $('#content').show('normal');  
+    }  
+     
+    return false;  
+      
+    }); 
+
+
+});
