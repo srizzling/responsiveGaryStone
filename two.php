9a4cb98baf5e335da889000cb3fd8f6f677160ca
Sriram Venkatesh <venksriram@gmail.com>
2013-08-13 03:38:46 -0400

    Added all Procedures to CMS

--- two.php
+++ two.php
@@ -1,3 +1,5 @@
+<?php require_once( 'couch/cms.php' ); ?>
+<cms:template title='Hernia Repair' />
 <div id="content">
 <div style="margin-top:50%;margin-left:5%; margin-right:15%">
 <font face="light" color="#aa0078" size="5%" >
@@ -5,8 +7,11 @@ HERNIA REPAIR
 </font>
 <p>
 <font face="Homepage" size="1%">
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+<cms:editable name='main_content' type='richtext'> 
+	
+</cms:editable>
 </font>
 </p>
 </div>
 </div>
+<?php COUCH::invoke(); ?>
